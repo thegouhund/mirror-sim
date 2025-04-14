6927bd8143631e092ef23082703db0b9cb690539
thegouhund <alexandresapu1@gmail.com>
2025-04-14 10:46:08 +0800

    add margin x

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import MirrorSimulation from "./MirrorSimulation";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center">
+    <main className="flex items-center justify-center mx-4">
       <MirrorSimulation />
     </main>
   );
